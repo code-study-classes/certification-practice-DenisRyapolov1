0dee4f0067b9486b76abc66d1ca3f81ba5ccba27
Ваше имя <@example.com>
2023-11-01 12:51:33 +0300

    numbers

--- numbers.js
+++ numbers.js
@@ -1,4 +1,3 @@
-import { string } from "yargs";
 
 function calculateDistance(x1, x2) {
     return Math.abs(x2-x1);
@@ -28,26 +27,35 @@ function calculateDigitSum(twoDigitNumber) {
     }
 }
 
-function swapHundredsAndTens(num) {
-   const a=num.toString();
-   const b=a.slice(0,1);
-   const c=a.slice(-1);
-};
-
-function getHundredsDigit() {
+function swapHundredsAndTens(twoDigitNumber) {
+    const dr = twoDigitNumber.toString();
+    const dd = dr.slice(0, 1);
+    const dg = dr.slice(-2, -1);
+    const ds = dr.slice(2);
+    const fd = dd + dg;
+    const rv = fd.split("").reverse().join("");
     
+    return Number(`${rv}${ds}`);
 };
 
-function getFullHours() {
-
+function getHundredsDigit(number) {
+    if (number <= 999) {
+      return 0;
+    }
+  
+    return Math.floor((number % 1000) / 100);
 };
 
-function getDayOfWeek() {
-
+function getFullHours(seconds) {
+    return Math.floor(seconds/3600);
 };
 
-function countSquares() {
+function getDayOfWeek(dayOfYear) {
+    return dayOfYear%7;
+};
 
+function countSquares(A, B, C) {
+    return Math.floor(A/C)*Math.floor(B/C);
 };
 
 export {
