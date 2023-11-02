1e42e689f3073b15b6dc2b400197b1d5182a6620
Ваше имя <@example.com>
2023-11-02 12:16:17 +0300

    strings

--- numbers.js
+++ numbers.js
@@ -1,63 +1,60 @@
-
 function calculateDistance(x1, x2) {
-    return Math.abs(x2-x1);
-};
+  return Math.abs(x2 - x1);
+}
 
 function calculateSegmentProduct(a, b, c) {
-    return Math.abs(a - c) * Math.abs(b - c);
-};
+  return Math.abs(a - c) * Math.abs(b - c);
+}
 
 function calculateKilobytes(num) {
-    const fileSizeInBytes=1024
-    return Math.floor(num/fileSizeInBytes);
-};
+  const fileSizeInBytes = 1024;
+  return Math.floor(num / fileSizeInBytes);
+}
 
-
-function calculateSegments(a,b) {
-    return Math.floor(a/b);
-};
+function calculateSegments(a, b) {
+  return Math.floor(a / b);
+}
 
 function calculateDigitSum(twoDigitNumber) {
-    if (twoDigitNumber >= 10 && twoDigitNumber <= 99) {
-        const tens = Math.floor(twoDigitNumber / 10);
-        const ones = twoDigitNumber % 10;
-        return tens + ones;
-    } else {
-        return '';
-    }
+  if (twoDigitNumber >= 10 && twoDigitNumber <= 99) {
+    const tens = Math.floor(twoDigitNumber / 10);
+    const ones = twoDigitNumber % 10;
+    return tens + ones;
+  }
+  return '';
 }
 
 function swapHundredsAndTens(twoDigitNumber) {
-    const dr = twoDigitNumber.toString();
-    const dd = dr.slice(0, 1);
-    const dg = dr.slice(-2, -1);
-    const ds = dr.slice(2);
-    const fd = dd + dg;
-    const rv = fd.split("").reverse().join("");
-    
-    return Number(`${rv}${ds}`);
-};
+  const dr = twoDigitNumber.toString();
+  const dd = dr.slice(0, 1);
+  const dg = dr.slice(-2, -1);
+  const ds = dr.slice(2);
+  const fd = dd + dg;
+  const rv = fd.split('').reverse().join('');
+
+  return Number(`${rv}${ds}`);
+}
 
 function getHundredsDigit(number) {
-    if (number <= 999) {
-      return 0;
-    }
-  
-    return Math.floor((number % 1000) / 100);
-};
+  if (number <= 999) {
+    return 0;
+  }
+
+  return Math.floor((number % 1000) / 100);
+}
 
 function getFullHours(seconds) {
-    return Math.floor(seconds/3600);
-};
+  return Math.floor(seconds / 3600);
+}
 
 function getDayOfWeek(dayOfYear) {
-    return dayOfYear%7;
-};
+  return dayOfYear % 7;
+}
 
 function countSquares(A, B, C) {
-    return Math.floor(A/C)*Math.floor(B/C);
-};
+  return Math.floor(A / C) * Math.floor(B / C);
+}
 
 export {
-    calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments, calculateDigitSum, swapHundredsAndTens, getHundredsDigit, getFullHours, getDayOfWeek, countSquares   
+  calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments, calculateDigitSum, swapHundredsAndTens, getHundredsDigit, getFullHours, getDayOfWeek, countSquares,
 };
--- strings.js
+++ strings.js
@@ -1,64 +1,84 @@
 function countUppercaseLetters(str) {
-    let count = 0;
-    let i = 0;
-    while (i < str.length) {
-        const char = str[i];
-        if (char >= 'A' && char <= 'Z') {
+  let count = 0;
+  let i = 0;
+  while (i < str.length) {
+    const char = str[i];
+    if (char >= 'A' && char <= 'Z') {
       count += 1;
     }
     i += 1;
-  };
+  }
   return count;
-};
+}
 
-function combineStrings(n1, n2, s1, s2) {  
-    let i=0;
-    let p=0;
-    let result='';
-    let resultat='';
-    s2=s2.split('').reverse().join('')
-    while (i<n1) {
-        result=result+s1[i];
-        i+=1;
-    }
-    while (p<n2) {
-        resultat=resultat+s2[p];
-        p+=1;
-    }
-    return result+resultat.split('').reverse().join('');
-};
+function combineStrings(n1, n2, s1, s2) {
+  let i = 0;
+  let p = 0;
+  let result = '';
+  let resultat = '';
+  const s2Rev = s2.split('').reverse().join('');
+  while (i < n1) {
+    result += s1[i];
+    i += 1;
+  }
+  while (p < n2) {
+    resultat += s2Rev[p];
+    p += 1;
+  }
+  return result + resultat.split('').reverse().join('');
+}
 
-function containsSubstring(s,s0) {
-    return s.includes(s0);
-};
+function containsSubstring(s, s0) {
+  return s.includes(s0);
+}
 
 function replaceSubstring(s, s1, s2) {
-    return s.replace(s1,s2);
-};
+  return s.replace(s1, s2);
+}
 
 function countWordsWithSameLetters(sentence) {
-    
-};
+  if (sentence.length === 0) { // если длина строки = 0
+    return 0;// возвращается 0
+  }
+  const str = sentence.toLowerCase().split(' ');// сделали строчку в одном регистре(все буковки маленькие), разделение строки на слова
+  let count = 0;// счётчик кол-ва слов
+  for (let i = 0; i < str.length; i += 1) { // цикл для перебора слова
+    if (str[i][str[i].length - 1] === str[i][0]) { // сравниваем последнюю и первую букву слова
+      count += 1; // если они совпали, то +1 счетчику
+    }
+  }
+  return count; // возвращаем счётчик
+}
 
 function countWordsWithA() {
-    
-};
+
+}
 
 function normalizeSpaces() {
-    
-};
+
+}
 
 function extractFileName() {
-    
-};
+
+}
 
 function encryptSentence() {
-    
-};
+
+}
 
 function checkBrackets() {
-    
-};
 
-export {countUppercaseLetters, combineStrings, containsSubstring, replaceSubstring, countWordsWithSameLetters, countWordsWithA, normalizeSpaces, extractFileName, encryptSentence, checkBrackets
-};    
+}
+
+export {
+  countUppercaseLetters,
+  combineStrings,
+  containsSubstring,
+  replaceSubstring,
+  countWordsWithSameLetters,
+  countWordsWithA,
+  normalizeSpaces,
+  extractFileName,
+  encryptSentence,
+  checkBrackets,
+};
